4e20aba494e276638e71a3501eb0ea231c46b610
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-05-28 16:55:41 +0200

    chore; added webserver impl in main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use actix_web::{get, Responder, HttpServer, HttpResponse, App};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            tauri::async_runtime::spawn(
+                HttpServer::new(|| App::new().service(hello))
+                    .bind(("127.0.0.1", 8080))?
+                    .run(),
+            );
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
