057a6cccb0ba0a88d44a95513c4ae33ebcc9639c
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-05-28 17:24:50 +0200

    chore; added logging functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use actix_web::{HttpServer, App};
+use log::info;
+use tauri_plugin_log::{LogTarget};
 
 mod api;
 
@@ -15,12 +17,19 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::default().targets([
+            LogTarget::LogDir,
+            LogTarget::Stdout,
+            LogTarget::Webview,
+        ]).build())
         .setup(|app| {
+            let address = ("127.0.0.1", 8080);
             tauri::async_runtime::spawn(
                 HttpServer::new(|| App::new().service(api::endpoints::hello))
-                    .bind(("127.0.0.1", 8080))?
+                    .bind(address)?
                     .run(),
             );
+            info!("web server started at http://{}:{}", address.0, address.1);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![greet])
