c80b547365755bfefde856d4eb6c331afa8ec903
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-05-29 16:23:10 +0200

    chore; finished styling the primary button to msfs guidelines

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
   import "carbon-components-svelte/css/all.css";
-  import { Button, Theme } from "carbon-components-svelte";
+  import { Button, Header, HeaderGlobalAction, HeaderUtilities, SkipToContent, Theme, Content } from "carbon-components-svelte";
   import Greet from './lib/Greet.svelte'
+
+  import SettingsAdjust from "carbon-icons-svelte/lib/SettingsAdjust.svelte";
+  import UserAvatarFilledAlt from "carbon-icons-svelte/lib/UserAvatarFilledAlt.svelte";
 </script>
 
+<Header company="IBM" platformName="Carbon Svelte">
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderUtilities>
+    <HeaderGlobalAction aria-label="Settings" icon={SettingsAdjust} />
+  </HeaderUtilities>
+</Header>
 
-<main class="container">
+<Content>
   <Theme
   theme="g90"
   tokens={{
     "interactive-01": "#00b4ffff",
     "hover-primary": "white",
-    "active-primary": "#9f1853",
     "ui-background": "transparent",
   }}
 />
   <Button>Primary button</Button>
-</main>
+</Content>
