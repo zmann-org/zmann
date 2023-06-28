9cb79b7be40795ea15b1a0ba04cd02a745d58edd
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-06-28 13:04:31 +0200

    prog; added logging and fmt

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,13 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_log::LogTarget;
 
 fn main() {
-  tauri::Builder::default()
-    .on_window_event(|e| {
-        if let tauri::WindowEvent::Resized(_) = e.event() {
-            std::thread::sleep(std::time::Duration::from_nanos(1));
-        }
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .on_window_event(|e| {
+            if let tauri::WindowEvent::Resized(_) = e.event() {
+                std::thread::sleep(std::time::Duration::from_nanos(1));
+            }
+        })
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
+                .build(),
+        )
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
