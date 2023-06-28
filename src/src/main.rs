9ca81653151b788c3232e78e2f9458e8a22894e5
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-06-28 12:53:08 +0200

    impl; added window resize sleep event to smoothen resize calls

--- src/src/main.rs
+++ src/src/main.rs
@@ -3,6 +3,11 @@
 
 fn main() {
   tauri::Builder::default()
+    .on_window_event(|e| {
+        if let WindowEvent::Resized(_) = e.event() {
+            std::thread::sleep(std::time::Duration::from_nanos(1));
+        }
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
