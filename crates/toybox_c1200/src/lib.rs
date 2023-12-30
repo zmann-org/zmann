e592e6193fd78ca83f40f0e8ccb3139740a7e25b
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-12-30 20:10:42 +0100

    fix; reverb impl

--- crates/toybox_c1200/src/lib.rs
+++ crates/toybox_c1200/src/lib.rs
@@ -289,64 +289,65 @@ impl Plugin for ToyboxC {
             let output = self.params.output_gain.smoothed.next();
 
             for (i, sample) in channel_samples.iter_mut().enumerate() {
+                let mut input = 0.0;
                 for playing_sample in &mut self.buffer {
-                    *sample += playing_sample.get_next_sample();
-
-                    // Update reverbs based on parameters
-                    let room_size_smoothed = &self.params.room_size.smoothed;
-                    let damping_smoothed = &self.params.damping.smoothed;
-                    let width_smoothed = &self.params.width.smoothed;
-
-                    // Update reverbs while parameters smooth
-                    if room_size_smoothed.is_smoothing() {
-                        self.freeverb.set_room_size(room_size_smoothed.next());
-                        self.moorer_reverb.set_room_size(room_size_smoothed.next());
-                    }
-                    if damping_smoothed.is_smoothing() {
-                        self.freeverb.set_damping(damping_smoothed.next());
-                        self.moorer_reverb.set_damping(damping_smoothed.next());
-                    }
-                    if width_smoothed.is_smoothing() {
-                        self.freeverb.set_width(width_smoothed.next());
-                        self.moorer_reverb.set_width(width_smoothed.next());
-                    }
-
-                    // Check if we should freeze the reverb
-                    let frozen = self.params.frozen.value();
-                    self.freeverb.set_frozen(frozen);
-                    self.moorer_reverb.set_frozen(frozen);
-
-                    // Get input/output gain
-                    let input_gain = self.params.input_gain.smoothed.next();
-                    let output_gain = self.params.output_gain.smoothed.next();
-
-                    // Process with reverb
-                    let input = *sample * input_gain;
-                    let frame_out = match self.params.reverb_type.value() {
-                        ReverbType::Freeverb => {
-                            if i % 2 == 0 {
-                                self.freeverb.tick((input, 0.0)).0
-                            } else {
-                                self.freeverb.tick((0.0, input)).1
-                            }
-                        }
-                        ReverbType::Moorer => {
-                            if i % 2 == 0 {
-                                self.moorer_reverb.tick((input, 0.0)).0
-                            } else {
-                                self.moorer_reverb.tick((0.0, input)).1
-                            }
-                        }
-                    };
-
-                    // Apply dry/wet, then output
-                    let dry_wet_ratio = self.params.dry_wet_ratio.smoothed.next();
-                    *sample = *sample * (1. - dry_wet_ratio) + frame_out * dry_wet_ratio;
-                    *sample *= output_gain;
+                    input += playing_sample.get_next_sample();
                 }
-
+            
+                // Update reverbs based on parameters
+                let room_size_smoothed = &self.params.room_size.smoothed;
+                let damping_smoothed = &self.params.damping.smoothed;
+                let width_smoothed = &self.params.width.smoothed;
+            
+                // Update reverbs while parameters smooth
+                if room_size_smoothed.is_smoothing() {
+                    self.freeverb.set_room_size(room_size_smoothed.next());
+                    self.moorer_reverb.set_room_size(room_size_smoothed.next());
+                }
+                if damping_smoothed.is_smoothing() {
+                    self.freeverb.set_damping(damping_smoothed.next());
+                    self.moorer_reverb.set_damping(damping_smoothed.next());
+                }
+                if width_smoothed.is_smoothing() {
+                    self.freeverb.set_width(width_smoothed.next());
+                    self.moorer_reverb.set_width(width_smoothed.next());
+                }
+            
+                // Check if we should freeze the reverb
+                let frozen = self.params.frozen.value();
+                self.freeverb.set_frozen(frozen);
+                self.moorer_reverb.set_frozen(frozen);
+            
+                // Get input/output gain
+                let input_gain = self.params.input_gain.smoothed.next();
+                let output_gain = self.params.output_gain.smoothed.next();
+            
+                // Process with reverb
+                input *= input_gain;
+                let frame_out = match self.params.reverb_type.value() {
+                    ReverbType::Freeverb => {
+                        if i % 2 == 0 {
+                            self.freeverb.tick((input, 0.0)).0
+                        } else {
+                            self.freeverb.tick((0.0, input)).1
+                        }
+                    }
+                    ReverbType::Moorer => {
+                        if i % 2 == 0 {
+                            self.moorer_reverb.tick((input, 0.0)).0
+                        } else {
+                            self.moorer_reverb.tick((0.0, input)).1
+                        }
+                    }
+                };
+            
+                // Apply dry/wet, then output
+                let dry_wet_ratio = self.params.dry_wet_ratio.smoothed.next();
+                *sample = input * (1. - dry_wet_ratio) + frame_out * dry_wet_ratio;
+                *sample *= output_gain;
+            
                 *sample *= output;
-
+            
                 self.buffer.retain(|e| !e.should_be_removed());
             }
         }
