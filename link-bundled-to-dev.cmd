baacc00b5de22f3f63949e07504689625b7e5795
AZ Software <66299945+AZProductions@users.noreply.github.com>
2023-12-31 23:02:58 +0100

    chore; get rid of admin popup message

--- link-bundled-to-dev.cmd
+++ link-bundled-to-dev.cmd
@@ -1,8 +1,3 @@
 @Echo off
-
-echo Please run this script as an administrator.
-pause
-
 cmd /c mklink /j "%COMMONPROGRAMFILES%\VST3\zmann-dev" "%~dp0target\bundled\"
-
 echo Successfully linked 'bundled' to 'dev'.
