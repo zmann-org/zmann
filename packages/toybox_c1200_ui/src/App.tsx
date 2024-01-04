fd5019ec6b8fa69071e2db24c1afbdd4160a50a1
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-01-04 18:05:31 +0100

    chore; remove unused imports

--- packages/toybox_c1200_ui/src/App.tsx
+++ packages/toybox_c1200_ui/src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
