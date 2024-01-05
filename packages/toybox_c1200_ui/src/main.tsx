a181cd1410908c41c606fe9adf46838a51114a90
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-01-05 20:17:39 +0100

    prog; setup himalaya-ui and disable-ui-feat

--- packages/toybox_c1200_ui/src/main.tsx
+++ packages/toybox_c1200_ui/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import StyledJsxRegistry from '@himalaya-ui/core/esm/use-context/registry'
+import ConfigProvider from '@himalaya-ui/core/esm/use-context/config-provider'
+import '@fontsource-variable/instrument-sans'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyledJsxRegistry>
+      <ConfigProvider>
+          <App />
+      </ConfigProvider>
+    </StyledJsxRegistry>
   </React.StrictMode>,
 )
