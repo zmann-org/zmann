799b071ce26ef0d07bd06c3e66c9fd36c516c0a2
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-02-17 13:33:14 +0100

    prog; move module component in separate package

--- packages/toybox_c1200_ui/app/page.tsx
+++ packages/toybox_c1200_ui/app/page.tsx
@@ -5,9 +5,7 @@ import {
   Grid,
   Select,
   Slider,
-  Card,
   useTheme,
-  useScale,
   ToggleList,
   Description,
   Button,
@@ -19,6 +17,7 @@ import {
 } from "@himalaya-ui/core/icons";
 import Badge from "@/lib/Badge";
 import Toybox from "@/lib/toybox";
+import Module from "@repo/ui/module";
 
 declare global {
   interface Window {
@@ -230,6 +229,7 @@ export default function Home() {
                 name="Reverb"
                 footer={
                   <Description
+                  style={{marginTop: '-24px'}}
                     title={
                       <span style={{ margin: "-2px 6px" }}>REVERB TYPE</span>
                     }
@@ -322,65 +322,3 @@ export default function Home() {
     </>
   );
 }
-
-interface ModuleProps {
-  children?: ReactNode;
-  footer?: ReactNode;
-  name?: string;
-}
-
-const Module: React.FC<ModuleProps> = ({ children, footer, name }) => {
-  const theme = useTheme();
-  const { SCALES } = useScale();
-  return (
-    <Card
-      style={{
-        height: "100%",
-        width: "100%",
-        background: theme.palette.accents_0 + "B2",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      {name && (
-        <header>
-          <div
-            style={{
-              border: `1px solid ${theme.palette.border}`,
-              backgroundColor: theme.palette.accents_0,
-              color: theme.palette.accents_5,
-              height: "auto",
-              lineHeight: "1.35em",
-              display: "inline-flex",
-              alignItems: "center",
-              fontSize: SCALES.font(0.8125),
-              padding: `${SCALES.font(0.32)} ${SCALES.font(0.5)} ${SCALES.font(
-                0.32
-              )} ${SCALES.font(0.5)}`,
-              width: "auto",
-              borderTopLeftRadius: `calc(${theme.style.radius} - 1px)`,
-              borderBottomRightRadius: theme.style.radius,
-              textTransform: "uppercase",
-              marginTop: "-1px",
-              marginLeft: "-1px",
-            }}
-          >
-            {name}
-          </div>
-        </header>
-      )}
-      <Card.Content>{children}</Card.Content>
-      {footer && (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "auto",
-          }}
-        >
-          {footer}
-        </div>
-      )}
-    </Card>
-  );
-};
--- packages/toybox_c1200_ui/next.config.js
+++ packages/toybox_c1200_ui/next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  transpilePackages: ["@repo/ui"],
 };
 
 module.exports = nextConfig;
