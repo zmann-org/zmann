4ad03b9580e3a87887bbf3ab754b75e21f189b26
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-03-30 12:44:46 +0100

    prog; add orchestron crate

---
+++ crates/orchestron/src/lib.rs
@@ -0,0 +1,119 @@
+use nih_plug::prelude::*;
+use std::sync::Arc;
+
+struct Orchestron {
+    params: Arc<OrchestronParams>,
+}
+
+#[derive(Params)]
+struct OrchestronParams {
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Orchestron {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(OrchestronParams::default()),
+        }
+    }
+}
+
+impl Default for OrchestronParams {
+    fn default() -> Self {
+        Self {
+            gain: FloatParam::new("Gain", util::db_to_gain(0.0), FloatRange::Skewed {
+                min: util::db_to_gain(-30.0),
+                max: util::db_to_gain(30.0),
+                factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+            })
+                .with_smoother(SmoothingStyle::Logarithmic(50.0))
+                .with_unit(" dB")
+                .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+                .with_string_to_value(formatters::s2v_f32_gain_to_db()),
+        }
+    }
+}
+
+impl Plugin for Orchestron {
+    const NAME: &'static str = "Orchestron";
+    const VENDOR: &'static str = "ZMANN";
+    const URL: &'static str = env!("CARGO_PKG_HOMEPAGE");
+    const EMAIL: &'static str = "info@zmann.org";
+
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+    // The first audio IO layout is used as the default. The other layouts may be selected either
+    // explicitly or automatically by the host or the user depending on the plugin API/backend.
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
+        AudioIOLayout {
+            main_input_channels: NonZeroU32::new(2),
+            main_output_channels: NonZeroU32::new(2),
+
+            aux_input_ports: &[],
+            aux_output_ports: &[],
+
+            names: PortNames::const_default(),
+        },
+    ];
+
+    const MIDI_INPUT: MidiConfig = MidiConfig::Basic;
+    const MIDI_OUTPUT: MidiConfig = MidiConfig::None;
+
+    const SAMPLE_ACCURATE_AUTOMATION: bool = true;
+
+    type SysExMessage = ();
+
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn initialize(
+        &mut self,
+        _audio_io_layout: &AudioIOLayout,
+        _buffer_config: &BufferConfig,
+        _context: &mut impl InitContext<Self>
+    ) -> bool {
+        // Resize buffers and perform other potentially expensive initialization operations here.
+        // The `reset()` function is always called right after this function. You can remove this
+        // function if you do not need it.
+        true
+    }
+
+    fn reset(&mut self) {
+        // Reset buffers and envelopes here. This can be called from the audio thread and may not
+        // allocate. You can remove this function if you do not need it.
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>
+    ) -> ProcessStatus {
+        for channel_samples in buffer.iter_samples() {
+            // Smoothing is optionally built into the parameters themselves
+            let gain = self.params.gain.smoothed.next();
+
+            for sample in channel_samples {
+                *sample *= gain;
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl Vst3Plugin for Orchestron {
+    const VST3_CLASS_ID: [u8; 16] = *b"zmann.orchestron";
+
+    // And also don't forget to change these categories
+    const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] = &[
+        Vst3SubCategory::Sampler,
+        Vst3SubCategory::Instrument,
+    ];
+}
+
+nih_export_vst3!(Orchestron);
