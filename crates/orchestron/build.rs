9d68b20b2e5dcf5e9fa198d22a5c35dd4ab58a07
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-04-04 20:53:12 +0200

    chore; always build ui to fix ci

--- crates/orchestron/build.rs
+++ crates/orchestron/build.rs
@@ -1,7 +1,7 @@
 use build_function::{ build, cargo_emit::{ rerun_if_changed, warning } };
 
 fn main() {
-    rerun_if_changed!(format!("{}/src/", env!("ORCHESTRON_UI")), "build.rs");
+    rerun_if_changed!("build.rs"); // format!("{}/src/", env!("ORCHESTRON_UI"))
 
     match build(env!("ORCHESTRON_UI"), std::env::var("PROFILE")) {
         Ok(_) => {
