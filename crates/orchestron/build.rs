2dbb981e573251f639ea391dde05161080dcfef4
AZ Software <66299945+AZProductions@users.noreply.github.com>
2024-04-05 20:35:22 +0200

    fix; actions workflow

--- crates/orchestron/build.rs
+++ crates/orchestron/build.rs
@@ -1,9 +1,7 @@
-use std::{fmt::format, fs::{self, canonicalize}};
-
 use build_function::{ build, cargo_emit::{ rerun_if_changed, warning } };
 
 fn main() {
-    rerun_if_changed!("build.rs"); // format!("{}/src/", env!("ORCHESTRON_UI"))
+    rerun_if_changed!("build.rs", format!("{}/src/", env!("ORCHESTRON_UI")));
 
     match build(env!("ORCHESTRON_UI"), std::env::var("PROFILE")) {
         Ok(_) => {
@@ -12,12 +10,5 @@ fn main() {
         Err(e) => {
             panic!("Build failed, {}", e);
         }
-    }
-
-    warning!("path: {:?}", canonicalize(env!("ORCHESTRON_UI")).unwrap());
-    let paths = fs::read_dir(format!("{:?}/dist", canonicalize(env!("ORCHESTRON_UI")))).unwrap();
-
-    for test in paths {
-        warning!("dist: {}", test.unwrap().path().display()); 
     }
 }
